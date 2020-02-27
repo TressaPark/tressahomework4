6d45e0e3ded3c1f2d7df604e0da2c381d3344e00
Tressa Park <tressapark@gmail.com>
2020-02-27 14:13:45 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+var questions = [
+    var score = 0;
+
+for(var i=0; i < questions.length; i++){}
+    var response = window.prompt()(questions[i].prompt);
+    if(response == questions[1].answer){
+        score++;
+        alert(Correct!")
+    }   else {
+        alert("Wrong!");
+    }
+{
+prompt: "What does HTML stand for?\n(a) Hyper Trainer Marking Language\n(b) 
+Hyper Text Marketing Language\n(c) Hyper Text Markup Language\n(d) Hyper Text Markup Leveler,
+answer: "c"
+},
+{
+prompt: "What is the name of the stylesheet that defines the presentation of an HTML or XML document?\n(a) CSS\n(b) 
+PHP\n(c) jQuery\n(d) Java,
+answer: "a"
+},
+{
+prompt: "What is the language or list of instructions that are executed by the computer (how JavaScript is built)?\n(a) Syntax\n(b) 
+Output\n(c) Scope\n(d) JSON,
+answer: "a"
+}
+}
+alert("You got " + score + "/" + questions.length);
